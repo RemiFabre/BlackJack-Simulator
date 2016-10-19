37f1895bf3115dfb9ad41cf132db1c6f9b0aee4f
RemiFabre <remifabre1800@gmail.com>
2016-10-19 10:50:40 +0200

    Adding 3 days of simus

--- logParser.py
+++ logParser.py
@@ -3,15 +3,25 @@ import sys
 infile, outfile = sys.argv[1], sys.argv[2]
 numbers = []
 positives_ev = []
+insurances = []
 max_ev = 0.0
 sum_positive_ev = 0.0
 sum_ev = 0.0
 key_string = "***Total EV : "
+key_string_insurance = "INFO - Insurance_EV = "
 with open(infile) as inf, open(outfile,"w") as outf:
     for line in inf :
         line = line.strip()
         index1 = line.find(key_string)
         if (index1 == -1) :
+            index1 = line.find(key_string_insurance)
+            if (index1 == -1) :
+                continue
+            index1 = index1 + len(key_string_insurance)
+            insurance_string = line[index1:].strip()
+            insurance = float(insurance_string)
+            if (insurance > 0) :
+                insurances.append(insurance)			
             continue
         index1 = index1 + len(key_string)
         index2 = line.find(",", index1)
@@ -34,6 +44,13 @@ with open(infile) as inf, open(outfile,"w") as outf:
         positives_ev_string = positives_ev_string + "{0:.3f}".format(ev) + ", "
     print ("positive evs : ", positives_ev_string)
 
+    print()
+    insurances.sort()
+    insurances_string = ""
+    for ev in insurances :
+        insurances_string = insurances_string + "{0:.3f}".format(ev) + ", "
+    print ("positive insurances : ", insurances_string)
+	
     print("max_ev = ", max_ev)
     print("sum_positive_ev = ", sum_positive_ev)
     print("sum_ev = ", sum_ev)
